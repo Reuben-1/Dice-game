baa4b982b5ece72d5cef7862ce370941667fbbd6
Chiemelie Nzelibe <chiinzelibe@gmail.com>
2024-05-21 11:57:42 +0200

    Fixed the the refresh me problem

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ var randomDice2= "images/dice"+ randomNumber2 +".png";
 document.querySelector(".img1").setAttribute("src",randomDice1);
 document.querySelector(".img2").setAttribute("src",randomDice2);
 
+
 if (randomNumber1 > randomNumber2){
     document.querySelector("h1").textContent="🚩 Player 1 Wins!";
 }
@@ -16,4 +17,3 @@ else if(randomNumber1 < randomNumber2){
 else{
     document.querySelector("h1").textContent="Draw";
 }
-
